b48d38c771f7e00ea871aecde17933f2c875a09b
Tom <marcinlowercase@gmail.com>
2025-10-27 17:28:19 -0600

    zero

---
+++ server.js
@@ -0,0 +1,68 @@
+//server.js
+import { join } from "https://deno.land/std@0.211.0/path/mod.ts";
+import { serveDir } from "https://deno.land/std@0.211.0/http/file_server.ts";
+import { contentType } from "https://deno.land/std@0.211.0/media_types/mod.ts";
+
+const ui_directory = "ui";
+
+const client_config_script = (request) => {
+  const client_device_corner_radius = parseFloat(
+    request.headers.get("device_corner_radius") || "0",
+  );
+
+  return `
+              <script>
+                window.APP_CONFIG = {
+                 client_device_corner_radius: ${client_device_corner_radius},
+                };
+
+              </script>
+            </head>
+            `;
+};
+
+const complete_response = async (file_path, request) => {
+  let file_content = await Deno.readTextFile(file_path);
+  const file_extension = file_path.split(".").pop();
+
+  if (file_extension === "html") {
+    file_content = file_content.replace(
+      "</head>",
+      client_config_script(request),
+    );
+  }
+
+  const response_headers = new Headers({
+    "content-type": contentType(`.${file_extension}`) ||
+      "application/octet-stream",
+  });
+
+  return new Response(file_content, {
+    status: 200,
+    headers: response_headers,
+  });
+};
+
+const handle_request = async (request) => {
+  const url = new URL(request.url);
+  const path_name = url.pathname;
+
+  try {
+    let file_path = "";
+    switch (path_name) {
+      case "/":
+        file_path = join(Deno.cwd(), ui_directory, "index.html");
+        return await complete_response(file_path, request);
+
+      default:
+        return serveDir(request, {
+          fsRoot: ui_directory,
+        });
+    }
+  } catch (error) {
+    console.warn(`File not found: ${path_name}`);
+    return new Response("Not Found", { status: 404 });
+  }
+};
+
+Deno.serve(handle_request);
---
+++ ui/function/index.js
@@ -0,0 +1,8 @@
+// index.js
+
+console.log("make by marcinlowercase");
+
+const client_device_corner_radius =
+  window.APP_CONFIG.client_device_corner_radius;
+
+console.log(`client_device_corner_radius: ${client_device_corner_radius}`);
